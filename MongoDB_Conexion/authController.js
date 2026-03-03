cd23f5d6bbfeff3597e9c38767a9a90eedec78ce
Dariel1977464 <111201117+DarielPerezCh9292@users.noreply.github.com>
2026-03-03 01:04:16 -0600

    Register y Login funcionan
    http://localhost:3000/api/auth/register
    http://localhost:3000/api/auth/login
    
    usando estas rutas en postman, se puede mandar informacion para registrar un Usuario o para logear en un usuario ya existente

--- MongoDB_Conexion/authController.js
+++ MongoDB_Conexion/authController.js
@@ -3,45 +3,81 @@ import { Usuario } from './schema.js';
 export const Registrar_Usuario = async (req, res) => {
     try {
         //const { username, Correo_Electronico, password } = req.body;
-        const { Correo_Electronico, Contrasena } = req.body;
+        const { Nombre_Completo, Correo_Electronico, Contrasena, Telefono, Es_Admin, Esta_Activo} = req.body;
 
-        // 1. Check if user already exists
         
-        const existingUser = await Usuario.findOne({ Correo_Electronico });
+        const Usuario_Existe = await Usuario.findOne({ Correo_Electronico });
         
         
         //res.status(200).json({ message: "The route works, DB is the problem" });
-
-
-        if (existingUser) {
-            return res.status(201).json({ message: "Usuario ya existe!"});
+        
+        //Checar si usuario Existe
+        if (Usuario_Existe) {
+            return res.status(400).json({ message: "Usuario ya existe!"});
         }
-        else{
-            return res.status(400).json({ 
-            message: "Usuario no existe!"
+        
+
+        //Ingresar Usuario
+        const Nuevo_Usuario = await Usuario.create({      
+            Nombre_Completo,
+            Correo_Electronico,
+            Contrasena,
+            Telefono,
+            Es_Admin,
+            Esta_Activo
         });
-        }
-
-        // 2. Create the user (In a real app, hash the password first!)
         /*
-        const newUser = await User.create({
-        username,
-        Correo_Electronico,
-        password 
-        });*/
-        const newUser = await Usuario.create({      
+        Ejemplo de info a llenar
+        const Nuevo_Usuario = await Usuario.create({      
             Nombre_Completo: 'Nombre Temp',
             Correo_Electronico: 'Temp@hotmail.com',
             Contrasena: 'minmin123',
             Telefono: '8123454522',
             Es_Admin: false,
             Esta_Activo: false
-        });
+        });*/
+        
 
         res.status(201).json({ 
             message: "Usuario Registrado!",
-            userId: newUser._id 
+            userId: Nuevo_Usuario._id 
         });
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
+
+export const Login_Usuario = async (req, res) => {
+    try {
+        const { Correo_Electronico, Contrasena } = req.body;
+
+        
+        
+        const Usuario_Existe_Correo = await Usuario.findOne({ Correo_Electronico });
+        
+        
+        
+
+        //Checar si usuario Existe
+        if (Usuario_Existe_Correo) {
+            //return res.status(201).json({ message: "Usuario si existe!"});
+
+            const Usuario_Existe_Contrasena = await Usuario.findOne({ Contrasena });
+
+            if (Usuario_Existe_Contrasena) {
+                return res.status(201).json({ message: "Usuario si existe, Login exitoso"});
+            }
+            else{
+                return res.status(400).json({ message: "Usuario si existe, contrasena equivocada!"});
+            }
+
+
+
+        }
+        else{
+            return res.status(400).json({ message: "Usuario no existe!"});
+        }
+
     } catch (error) {
         res.status(500).json({ message: error.message });
     }
--- MongoDB_Conexion/authRoutes.js
+++ MongoDB_Conexion/authRoutes.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import { Registrar_Usuario } from './authController.js';
+import { Login_Usuario } from './authController.js';
 
 const router = express.Router();
 
 // This maps a POST request to our function
 router.post('/register', Registrar_Usuario);
+router.post('/login', Login_Usuario);
 
 export default router;
