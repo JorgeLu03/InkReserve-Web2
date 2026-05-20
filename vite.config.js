ad5c908fabf4627e4195d1dc0efed99419f7eb52
Jorge Luna <jalp._@hotmail.com>
2026-05-20 04:41:15 -0600

    Correccion de variables

--- vite.config.js
+++ vite.config.js
@@ -2,15 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-const railwayHost = process.env.RAILWAY_PUBLIC_DOMAIN
-const allowedHosts = ['localhost', '127.0.0.1']
-if (railwayHost) allowedHosts.push(railwayHost)
-
 export default defineConfig({
   plugins: [react()],
   server: {
     host: '0.0.0.0',
-    allowedHosts,
+    allowedHosts: true,
     proxy: {
       '/api': {
         target: 'http://localhost:3000',
@@ -20,6 +16,6 @@ export default defineConfig({
   },
   preview: {
     host: '0.0.0.0',
-    allowedHosts,
+    allowedHosts: true,
   },
 })
